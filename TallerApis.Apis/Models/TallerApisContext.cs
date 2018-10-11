80988a2ee1d38233965e198dc448afc96f09784b
SEBASTIAN <SEBASTIAN@PORTATIL_HP>
2018-10-10 20:17:45 -0500

    Base de datos

---
+++ TallerApis.Apis/Models/TallerApisContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TallerApis.Apis.Models
+{
+    public class TallerApisContext : DbContext
+    {
+        public TallerApisContext() : base("TallerApisConnection")
+        {
+
+        }
+
+        public DbSet<Publicacion> Publicaciones { get; set; }
+    }
+}
